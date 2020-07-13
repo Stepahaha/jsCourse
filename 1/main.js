d727f4de5a5b669e46876eeaca3cfae1a2b2c63d
Stepan <test@mail.com>
2020-07-13 14:16:36 +0300

    Сделал дз по магазину с 4-го урока

--- 1/main.js
+++ 1/main.js
@@ -18,12 +18,9 @@ function start(){
   }
   time = +prompt("Сколько время сейчас?", 9);
   discount = confirm('Нужна скидка?');
-  if(discount == true){
-    price *= 0.8;
-  };
 }
 
-start();
+// start();
 
 let mainList = {
  budget: money,
@@ -32,63 +29,76 @@ let mainList = {
  employers: {},
  open: false, 
  discount: discount,
- price: price
-};
-
-function chooseGoods(){
-  for (let i = 0; i < 5; i++) {
-    let a = prompt("Что будем продавать?");
-    if (isNaN(a) == true && a.length < 50 && a != '' && a != null){
-      console.log('okay!');
-      mainList.shopGoods[i] = a; 
-      // Validate prompt 
-    } else { // get correct prompt
-      i -= 1;
+ price: price,
+ shopItems: [], 
+ chooseGoods: function chooseGoods(){
+    for (let i = 0; i < 5; i++) {
+      let a = prompt("Что будем продавать?");
+      if (isNaN(a) == true && a.length < 50 && a != '' && a != null){
+        console.log('okay!');
+        mainList.shopGoods[i] = a; 
+        // Validate prompt 
+      } else { // get correct prompt
+        i -= 1;
+      }
+    };  
+  },
+  getEmp: function getEmp(){
+    for(let i = 1; i < 5; i++){
+      let employ = prompt('Как зовут работника?');
+      while (isNaN(employ) == false|| employ == null || employ == ''){
+        employ = prompt('Как зовут работника?');
+      }
+      mainList.employers[i] = employ;
     }
-   };  
-}
-
-
-
-function getEmp(){
-  for(let i = 1; i < 5; i++){
-    let employ = prompt('Как зовут работника?');
-    while (isNaN(employ) == false|| employ == null || employ == ''){
-      employ = prompt('Как зовут работника?');
+  },
+  makeDiscount: function makeDiscount(){
+    if (mainList.discount){
+      mainList.price *= 0.8;
     }
-    mainList.employers[i] = employ;
+  },
+  workTime: function workTime(time){
+    if (time < 0){
+      console.log('Такого не может быть');
+     }else if (time > 8 && time < 20){
+      console.log('Время работать!');
+      mainList.open = true;
+      }else if (time < 24){
+       console.log('Время уже позднее - магазин закрыт');
+       mainList.open = false;
+       }else{
+        console.log('В сутках только 24 часа');
+       }
+  },
+  calcBudget: function calcBudget(money){
+    alert('Ваш бюджет на один день: ' + String(Number(money) / 30));
+  },
+  chooseShopItems: function chooseShopItems() {
+    let items = prompt('Перечислите через запятую товары', '');
+    while (isNaN(items) == false || items == null || items == ''){
+      items = prompt('Перечислите через запятую товары', ''); 
+    }
+    mainList.shopItems = items.split(',');
+    mainList.shopItems.push(prompt('Добавь ещё один', ''));
+    mainList.shopItems.sort();  
+   },
+  showCatalog: function showCatalog() {
+    alert('У нас вы можете приобрести:')
+    this.shopItems.forEach(element => {
+      alert(element);
+    });
+  },
+  showInfo: function showInfo() {
+    alert('Наш магазин включает в себя - ')
+    for( let key in mainList ) {
+      alert(key);
+    }
   }
-}
-
-
-
-
-function calcBudget(money){
-  alert('Ваш бюджет на один день: ' + String(Number(money) / 30));
-}
+};
 
 
 
 console.log(mainList)
-
-function workTime(time){
-  if (time < 0){
-    console.log('Такого не может быть');
-   }else if (time > 8 && time < 20){
-    console.log('Время работать!');
-    mainList.open = true;
-    }else if (time < 24){
-     console.log('Время уже позднее - магазин закрыт');
-     mainList.open = false;
-     }else{
-      console.log('В сутках только 24 часа');
-     }
-}
-
-chooseGoods();
-getEmp();
-calcBudget(mainList.budget);
-workTime(time);
 
 
 
